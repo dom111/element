07b879df29414f3880030282c412d9d31cc7bbef
dom111 <dom111@users.noreply.github.com>
2022-08-28 22:45:45 +0100

    Add `emit`, `emitCustom` and `once` functions.

--- src/Element.ts
+++ src/Element.ts
@@ -1,5 +1,20 @@
 import { parse } from 'css-what';
 
+export const emit = (element: EventTarget, event: Event): boolean =>
+  element.dispatchEvent(event);
+
+export const emitCustom = <T extends any[] = any[]>(
+  element: EventTarget,
+  eventName: string,
+  ...params: T
+): boolean =>
+  emit(
+    element,
+    new CustomEvent<T>(eventName, {
+      detail: params,
+    })
+  );
+
 export const empty = (element: HTMLElement): void => {
   while (element.hasChildNodes()) {
     element.firstChild.remove();
@@ -47,52 +62,84 @@ export const on:
   | (<K extends keyof GlobalEventHandlersEventMap>(
       target: EventTarget,
       event: K,
-      handler: (event: GlobalEventHandlersEventMap[K]) => void,
+      listener: (event: GlobalEventHandlersEventMap[K]) => void,
       options?: boolean | AddEventListenerOptions
     ) => any)
   | ((
       element: EventTarget,
-      type: string,
+      event: string,
       listener: EventListenerOrEventListenerObject,
       options?: boolean | AddEventListenerOptions
     ) => any) = (target, event, handler, options): void =>
   target.addEventListener(event, handler, options);
 
+export const once:
+  | (<K extends keyof GlobalEventHandlersEventMap>(
+      target: EventTarget,
+      event: K,
+      listener: (event: GlobalEventHandlersEventMap[K]) => void,
+      options?: boolean | AddEventListenerOptions
+    ) => any)
+  | ((
+      element: EventTarget,
+      event: string,
+      listener: EventListenerOrEventListenerObject,
+      options?: boolean | AddEventListenerOptions
+    ) => any) = (target, event, handler, options): void =>
+  on(event, handler, { ...options, once: true });
+
 export const onEach:
   | (<K extends keyof GlobalEventHandlersEventMap>(
       target: EventTarget,
       events: K[],
-      handler: (event: GlobalEventHandlersEventMap[K]) => void,
+      listener: (event: GlobalEventHandlersEventMap[K]) => void,
       options?: boolean | AddEventListenerOptions
     ) => any)
   | ((
       target: EventTarget,
-      type: string[],
+      events: string[],
       listener: EventListenerOrEventListenerObject,
       options?: boolean | AddEventListenerOptions
     ) => any) = (target, events, handler, options): any => {
   events.forEach((event) => on(target, event, handler, options));
 };
 
+export const s = <T extends HTMLElement = HTMLElement>(html: string): T => {
+  const container = document.createElement('div');
+
+  container.innerHTML = html;
+
+  return container.firstElementChild as T;
+};
+
 export const t = (content: string): Text => document.createTextNode(content);
 
-export class Element<T extends HTMLElement = HTMLElement> {
+type CustomEventMap = {
+  [key: string]: any[];
+};
+
+export class Element<
+  T extends HTMLElement = HTMLElement,
+  M extends CustomEventMap = CustomEventMap
+> {
   #element: T;
 
   constructor(element: T) {
     this.#element = element;
   }
 
-  static fromSelector(selector: string, ...childNodes: Node[]): Element {
+  static fromSelector<
+    T extends HTMLElement = HTMLElement,
+    M extends CustomEventMap = CustomEventMap
+  >(selector: string, ...childNodes: Node[]): Element<T, M> {
     return new Element(h(selector, ...childNodes));
   }
 
-  static fromString(html: string): Element {
-    const container = document.createElement('div');
-
-    container.innerHTML = html;
-
-    return new Element(container.firstElementChild as HTMLElement);
+  static fromString<
+    T extends HTMLElement = HTMLElement,
+    M extends CustomEventMap = CustomEventMap
+  >(html: string): Element<T, M> {
+    return new Element(s(html));
   }
 
   addClass(...classes: string[]): void {
@@ -113,17 +160,33 @@ export class Element<T extends HTMLElement = HTMLElement> {
     return this.#element;
   }
 
+  emit(event: Event): boolean {
+    return emit(this.element(), event);
+  }
+
+  emitCustom<T extends any[] = any[]>(
+    eventName: string,
+    ...params: T
+  ): boolean {
+    return emitCustom<T>(this.element(), eventName, ...params);
+  }
+
   empty(): void {
     empty(this.element());
   }
 
   on<K extends keyof GlobalEventHandlersEventMap>(
     event: K,
-    handler: (event: GlobalEventHandlersEventMap[K]) => void,
+    listener: (event: GlobalEventHandlersEventMap[K]) => void,
+    options?: boolean | AddEventListenerOptions
+  ): void;
+  on<K extends keyof M>(
+    event: K,
+    listener: (event: CustomEvent<M[K]>) => void,
     options?: boolean | AddEventListenerOptions
   ): void;
   on(
-    type: string,
+    event: string,
     listener: EventListenerOrEventListenerObject,
     options?: boolean | AddEventListenerOptions
   ): void;
@@ -131,13 +194,37 @@ export class Element<T extends HTMLElement = HTMLElement> {
     on(this.element(), event, handler, options);
   }
 
+  once<K extends keyof GlobalEventHandlersEventMap>(
+    event: K,
+    listener: (event: GlobalEventHandlersEventMap[K]) => void,
+    options?: boolean | AddEventListenerOptions
+  ): void;
+  once<K extends keyof M>(
+    event: K,
+    listener: (event: CustomEvent<M[K]>) => void,
+    options?: boolean | AddEventListenerOptions
+  ): void;
+  once(
+    event: string,
+    listener: EventListenerOrEventListenerObject,
+    options?: boolean | AddEventListenerOptions
+  ): void;
+  once(event, handler, options): void {
+    once(this.element(), event, handler, options);
+  }
+
   onEach<K extends keyof GlobalEventHandlersEventMap>(
     events: K[],
-    handler: (event: GlobalEventHandlersEventMap[K]) => void,
+    listener: (event: GlobalEventHandlersEventMap[K]) => void,
+    options?: boolean | AddEventListenerOptions
+  ): void;
+  onEach<K extends keyof M>(
+    events: K[],
+    listener: (event: CustomEvent<M[K]>) => void,
     options?: boolean | AddEventListenerOptions
   ): void;
   onEach(
-    type: string[],
+    events: string[],
     listener: EventListenerOrEventListenerObject,
     options?: boolean | AddEventListenerOptions
   ): void;
--- tests/unit/Example.test.ts
+++ tests/unit/Example.test.ts
@@ -1,5 +1,21 @@
+import Element from '../../src/Element';
+
 describe('Example', () => {
   describe('property', () => {
-    test.todo('TODO');
+    test.todo('TODO', async () => {
+      const element = Element.fromString<
+        HTMLDivElement,
+        {
+          foo: [string, number, HTMLElement];
+        }
+      >('<div></div>');
+
+      element.on('foo', (event) => {
+        const [a, b, c] = event.detail;
+
+        // Error on unsupported types
+        document.body.append(a, b, c);
+      });
+    });
   });
 });
